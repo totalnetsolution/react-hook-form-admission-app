1bcc02c1a0e0bd4522a215d58b19cfe4e59aaa96
muhammad-danish <totalnetsolution2@gmail.com>
2024-10-02 09:15:45 +0500

    admission app completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,21 +3,9 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-info text-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-info">
       <div className="container">
         <Link className="navbar-brand" to="/">Admission Form</Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
               <Link className="nav-link" to="/">Admission Form</Link>
@@ -27,7 +15,6 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
-      </div>
     </nav>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { store } from './app/store';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';  
+import './index.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <Router>
         <App />
-      </BrowserRouter>
+      </Router>
     </Provider>
   </React.StrictMode>
 );
